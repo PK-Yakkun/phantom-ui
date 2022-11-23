a3da6f733d014fdc20899426209bcc3accedf251
PK-Yakkun <mokkun515@gmail.com>
2022-11-24 00:52:28 +0900

    add: theme作成

---
+++ packages/phantom-ui/src/theme/index.ts
@@ -0,0 +1,11 @@
+import { palette } from "./settings/palettes";
+import { fonts } from "./settings/fonts";
+import { breakpoints } from "./settings/breakpoints";
+import { spacings } from "./settings/spacings";
+
+export const theme = {
+  color: palette,
+  font: fonts,
+  breakpoint: breakpoints,
+  spacing: spacings,
+};
---
+++ packages/phantom-ui/src/theme/settings/palettes.ts
@@ -0,0 +1,6 @@
+export const palette = {
+  body: "#fff",
+  primary: "#9a9fac",
+  secondary: "#f3f3f3",
+  typography: "#5f626c",
+};
